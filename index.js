0c448f14362b0ef5a677b4690ff26aad97bbe14f
sungseanpark <sungseanpark@gmail.com>
2024-04-14 19:58:12 -0700

    Complete part 8.5

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -125,10 +125,16 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-        if( !args.author){
+        if( !args.author && !args.genre){
             return books
         }
-        return books.filter(b => b.author === args.author)
+        if( !args.genre ){
+            return books.filter(b => b.author === args.author)
+        }
+        if (!args.author ){
+            return books.filter(b => b.genres.includes(args.genre))
+        }
+        return books.filter(b => b.author === args.author && b.genres.includes(args.genre))
     },
     allAuthors: () => authors
   },
