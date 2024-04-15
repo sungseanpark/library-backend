c9914065ff07232c21ce4eef808c587a6e67fc98
sungseanpark <sungseanpark@gmail.com>
2024-04-14 20:54:00 -0700

    Complete part 8.7

--- index.js
+++ index.js
@@ -128,6 +128,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ):Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ):Author
   }
 `
 
@@ -166,6 +170,16 @@ const resolvers = {
         }
 
         return book
+    },
+    editAuthor: (root, args) => {
+        const author = authors.find(a => a.name === args.name)
+        if(!author){
+            return null
+        }
+
+        const updatedAuthor = {...author, born: args.setBornTo}
+        authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+        return updatedAuthor
     }
   }
 }
